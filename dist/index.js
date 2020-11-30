ea17e2c672f00944c9c43cdc19c5253ee1a1d4cd
Antoine Augusti <antoine.augusti@cds-snc.ca>
2020-11-29 21:27:45 -0500

    Update pr-bot to send PRs to new repo (#7)
    * Update pr-bot to send PRs to new repo
    
    * Update message
    
    * Check Terraform infrastructure version as well
    
    * Refactor

--- dist/index.js
+++ dist/index.js
@@ -36,20 +36,30 @@ const getCommitMessages = async (repo, sha) => {
   return commits.slice(0, index).map(c => `- [${c.commit.message.split("\n\n")[0]}](${c.html_url}) by ${c.commit.author.name}`)
 }
 
-const getHeadSha = async (repo) => {
+const getHeadSha = async (repo, branch = 'master') => {
   const { data: data } = await octokit.repos.getBranch({
     owner: 'cds-snc',
-    repo: repo,
-    branch: 'master'
+    repo,
+    branch,
   });
   return data.commit.sha;
+}
+
+const getLatestTag = async (repo) => {
+  const {data: [latestTag]} = await octokit.repos.listTags({
+    owner: 'cds-snc',
+    repo,
+    per_page: 1,
+  });
+
+  return latestTag.name;
 }
 
 async function closePRs() {
    // Close old auto PRs
    const {data: prs} = await octokit.pulls.list({
      owner: 'cds-snc',
-     repo: 'notification-tf',
+     repo: 'notification-manifests',
      state: 'open'
    });
 
@@ -57,61 +67,65 @@ async function closePRs() {
      if(pr.title.startsWith("[AUTO-PR]")) {
        await octokit.pulls.update({
          owner: 'cds-snc',
-         repo: 'notification-tf',
+         repo: 'notification-manifests',
          pull_number: pr.number,
          state: "closed"
        });
        await octokit.git.deleteRef({
          owner: 'cds-snc',
-         repo: 'notification-tf',
+         repo: 'notification-manifests',
          ref: `heads/${pr.head.ref}`
        });
      }
    })
 }
 
-async function hasChangesOnStagingTfRepo() {
-    pullsSearchDefinition = {
-        owner: 'cds-snc',
-        sort: 'updated',
-        direction: 'desc',
-        // Not possible to target merged PRs, should filter on merged_at instead
-        state: 'closed',
-        page: 1
-    }
-    const prodPulls = await octokit.pulls.list({
-        ...pullsSearchDefinition,
-        repo: 'notification-tf',
-    })
-    const stagingPulls = await octokit.pulls.list({
-        ...pullsSearchDefinition,
-        repo: 'notification-staging-tf',
-    })
+async function isNotLatestManifestsVersion() {
+    const { data: data } = await octokit.repos.getContents({
+      owner: 'cds-snc',
+      repo: 'notification-manifests',
+      path: 'env/production/kustomization.yaml'
+    });
 
-    const mergedProdPulls = prodPulls['data'].filter(pr => pr.merged_at !== null);
-    const mergedStagingPulls = stagingPulls['data'].filter(pr => pr.merged_at !== null);
+    const fileContent = Base64.decode(data.content);
+    const prodVersion = fileContent.match(/notification-manifests\/\/base\?ref=(.*)/)[1];
 
-    const prodLastProdPR = Date.parse(mergedProdPulls[0].merged_at);
-    const stagingLastPR = Date.parse(mergedStagingPulls[0].merged_at);
-    return stagingLastPR > prodLastProdPR;
+    const latestVersion = await getLatestTag('notification-manifests');
+
+    return prodVersion != latestVersion;
+}
+
+async function isNotLatestTerraformVersion() {
+    const { data: data } = await octokit.repos.getContents({
+      owner: 'cds-snc',
+      repo: 'notification-terraform',
+      path: '.github/workflows/merge_to_main_production.yml'
+    });
+
+    const fileContent = Base64.decode(data.content);
+    const prodVersion = fileContent.match(/INFRASTRUCTURE_VERSION: '(.*)'/)[1];
+
+    const latestVersion = (await getLatestTag('notification-terraform')).replace('v', '');
+
+    return prodVersion != latestVersion;
 }
 
 async function run() {
     const { data: data } = await octokit.repos.getContents({
       owner: 'cds-snc',
-      repo: 'notification-tf',
-      path: 'manifests/overlays/eks/kustomization.yaml'
+      repo: 'notification-manifests',
+      path: 'env/production/kustomization.yaml'
     });
 
     const { data: issueData } = await octokit.repos.getContents({
       owner: 'cds-snc',
-      repo: 'notification-tf',
+      repo: 'notification-manifests',
       path: '.github/PULL_REQUEST_TEMPLATE.md'
     });
 
     const apiSha = await getHeadSha("notification-api");
     const adminSha = await getHeadSha("notification-admin");
-    const tfSha = await getHeadSha("notification-tf");
+    const manifestsSha = await getHeadSha("notification-manifests", "main");
 
     fileContent = YAML.parse(Base64.decode(data.content));
     issueContent = Base64.decode(issueData.content);
@@ -136,35 +150,39 @@ async function run() {
       const apiMsgs = await getCommitMessages("notification-api", oldApiSha)
 
       let logs = `ADMIN: \n\n ${adminMsgs.join("\n")} \n\n API: \n\n ${apiMsgs.join("\n")}`
-      if (await hasChangesOnStagingTfRepo()) {
-        logs = `⚠️ There was a merged pull request in [notification-staging-tf](https://github.com/cds-snc/notification-staging-tf) since last deploy in production! Check first that you don't need to change the infrastructure here. \n\n ${logs}`
+      if (await isNotLatestManifestsVersion()) {
+        logs = `⚠️ **The production version of manifests is behind the latest staging version. Consider upgrading to the latest version before merging this pull request.** \n\n ${logs}`
+      }
+
+      if (await isNotLatestTerraformVersion()) {
+        logs = `⚠️ **The production version of the Terraform infrastructure is behind the latest staging version. Consider upgrading to the latest version before merging this pull request.** \n\n ${logs}`
       }
 
       branchName = `release-${new Date().getTime()}`
 
       await octokit.git.createRef({
         owner: 'cds-snc',
-        repo: 'notification-tf',
+        repo: 'notification-manifests',
         ref: `refs/heads/${branchName}`,
-        sha: tfSha
+        sha: manifestsSha
       });
 
       await octokit.repos.createOrUpdateFile({
         owner: 'cds-snc',
-        repo: 'notification-tf',
+        repo: 'notification-manifests',
         branch: branchName,
         sha: data.sha,
-        path: 'manifests/overlays/eks/kustomization.yaml',
+        path: 'env/production/kustomization.yaml',
         message: `Updated manifests to admin:${adminSha.slice(0,7)} and api:${apiSha.slice(0,7)}`,
         content: newBlob
       })
 
       await octokit.pulls.create({
         owner: 'cds-snc',
-        repo: 'notification-tf',
+        repo: 'notification-manifests',
         title: `[AUTO-PR] Automatically generated new release ${new Date().toISOString()}`,
         head: branchName,
-        base: 'master',
+        base: 'main',
         body: issueContent.replace("> Give details ex. Security patching, content update, more API pods etc", logs),
         draft: true
       });
