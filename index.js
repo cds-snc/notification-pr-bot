a801f7fdbebb9d91a71fe49cbc0546fb049a763c
Ben Larabie <ben.larabie@cds-snc.ca>
2025-01-07 13:08:10 -0500

    first crack at admin update

--- index.js
+++ index.js
@@ -7,30 +7,40 @@ const PROJECTS = [
   {
     repoName: "notification-api",
     manifestFile: ".github/workflows/merge_to_main_production.yaml",
+    helmfileOverride: "helmfile/overrides/production.yaml",
+    helmfileTagKey: "API_DOCKER_TAG",
     ecrUrl: "${PRODUCTION_ECR_ACCOUNT}.dkr.ecr.ca-central-1.amazonaws.com/notify",
     ecrName: "api-lambda",
   },
   {
     repoName: "notification-api",
     manifestFile: "env/production/kustomization.yaml",
+    helmfileOverride: "helmfile/overrides/production.yaml",
+    helmfileTagKey: "API_DOCKER_TAG",
     ecrUrl: AWS_ECR_URL,
     ecrName: "notify-api",
   },
   {
     repoName: "notification-admin",
     manifestFile: "env/production/kustomization.yaml",
+    helmfileOverride: "helmfile/overrides/production.yaml",
+    helmfileTagKey: "ADMIN_DOCKER_TAG",
     ecrUrl: AWS_ECR_URL,
     ecrName: "notify-admin",
   },
   {
     repoName: "notification-document-download-api",
     manifestFile: "env/production/kustomization.yaml",
+    helmfileOverride: "helmfile/overrides/production.yaml",
+    helmfileTagKey: "DOCUMENT_DOWNLOAD_DOCKER_TAG",
     ecrUrl: AWS_ECR_URL,
     ecrName: "notify-document-download-api",
   },
   {
     repoName: "notification-documentation",
     manifestFile: "env/production/kustomization.yaml",
+    helmfileOverride: "helmfile/overrides/production.yaml",
+    helmfileTagKey: "DOCUMENTATION_DOCKER_TAG",
     ecrUrl: AWS_ECR_URL,
     ecrName: "notify-documentation",
   },
@@ -83,6 +93,18 @@ async function hydrateWithSHAs(projects) {
       const re = new RegExp(`${project.ecrName}:\\S*`, "g");
       project.oldUrl = originalFileContents.match(re)[0]
       project.oldSha = getSha(project.oldUrl);
+
+      // Patch the helmfile tags
+      const helmfileContent = await getContents(
+        "notifiation-manifests",
+        project.helmfileOverride
+      );
+
+      const helmfileContents = Base64.decode(helmfileContent.content)
+      const helmfileRe = new RegExp(`${project.helmfileTagKey} : "(.*?)"`, "g")
+      project.oldHelmfileTag = helmfileContents.match(helmfileRe)[0]
+      project.oldHelmfileSha = getSha(project.oldHelmfileTag);
+
       return project;
     })
   );
@@ -90,6 +112,10 @@ async function hydrateWithSHAs(projects) {
 
 function updateReleaseSha(content, project) {
   return content.replace(`${project.ecrName}:${project.oldSha}`, `${project.ecrName}:${shortSha(project.headSha)}`)
+}
+
+function updateHelmfileSha(conent,project) {
+  return content.replace(`${project.helmfileTagKey}: "${project.oldSha}"`, `${project.helmfileTagKey}: "${shortSha(project.headSha)}"`)
 }
 
 // Main ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -116,6 +142,7 @@ async function run(projects) {
     var fileContents = Base64.decode(releaseContent.content)
     projectsForFile.forEach((project) => {
       fileContents = updateReleaseSha(fileContents, project)
+      helmfileContents = updateHelmfileSha(helmfileContents, project)
     })
 
     const newReleaseContentBlob = Base64.encode(fileContents);
